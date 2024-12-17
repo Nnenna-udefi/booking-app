84098067019d46b928b576664f32cc8108bf3983
Nnenna-udefi <miriamudefi@gmail.com>
2024-12-17 11:35:23 +0100

    updated backend link

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
+        destination: "https://dc-braiding-gqwc.onrender.com/api/:path*",
       },
     ];
   },
